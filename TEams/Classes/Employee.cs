7fced32e2b3823093f64cf611e7a563e71b388ae
Laura Brenner <laura.brenner@ymail.com>
2021-05-21 13:09:47 -0400

    employee class added

---
+++ TEams/Classes/Employee.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TEams.Classes
+{
+    class Employee
+    {
+        public long EmployeeId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public double Salary { get; set; }//constant 60,000 by default
+        public Department Department { get; set; }
+        public string HireDate { get; set; }
+        public string FullName
+        {
+            get
+            {
+                return this.FirstName + this.LastName;
+            }
+        }
+
+        public Employee (long employeeId, string firstName, string lastName, string email, Department department, string hireDate)
+        {
+            this.EmployeeId = employeeId;
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Email = email;
+            this.Department = department;//might work?
+            this.HireDate = hireDate;
+        }
+
+        public Employee()
+        {
+            
+        }
+
+        public double RaiseSalary(double percent)
+        {
+            double raisedSalary;
+            raisedSalary = (this.Salary * percent) + this.Salary;
+            return raisedSalary;
+        }
+    }
+}
